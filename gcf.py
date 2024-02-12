80db7cfc2451d314f47868700c001e81b7098d03
ddeanh <deanh25@nycstudents.net>
2024-02-12 12:29:33 -0500

    fajfajfjfa

--- gcf.py
+++ gcf.py
@@ -1,24 +1,13 @@
 number = float(input("What is your number?"))
 number2 = float(input("What is your second number?"))
+
 num = int(number)
 num2 = int(number2)
 factors = []
-for i in range(1,int(num2/2)):
+for i in range(1, number+1):
     if num%1 == 0 and num2%i ==0:
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for i in factors:
-    print (i)
+        
+        
+        for i in factors:
+            print (i)
