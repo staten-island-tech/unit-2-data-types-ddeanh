a02e7e3268911d63d9e2fc50b5695b95ce02dd18
ddeanh <deanh25@nycstudents.net>
2024-03-06 12:31:03 -0500

    asdf

--- numberguessgame.py
+++ numberguessgame.py
@@ -1,23 +1,19 @@
 import  random
-number = random.randint(0,200)
+number = random.randint(0,10)
 history = []
 guess_history = history
 
-while True: 
+while True:
     guess = int(input("Guess a number"))
     history.append(guess)
     if guess == number:
-        print ("Congratulations, you got the number correct.")
+        print ("Congratulations, you got the number correct. Here are your previous guesses." ,print(guess_history))
         break
-    print(guess_history)
+    print(guess_history),
 
-q   elif guess != number:
-print ("Your guess is lower than the actual number")
-if guess > number:
-            print ("Your guess is higher than the actual number")   
-else:
-            print (guess, "is lower than the actual number ")
-
-
-print (history)
-
+if guess != number:
+    if guess > number:
+        print( guess,"is higher than the actual number")
+if guess != number:
+    if guess < number:
+        print( guess, "is lower than the actual number")
